ea52d7f469658d91e76a59519af875dff83ff1ff
aprakob <ownerlis83@gmail.com>
2017-07-11 23:05:49 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,10 +1,9 @@
 <?php
-$access_token = '5BYUK9okWxGTtZujRx/DtuHl7kEGeQU9lyW+cuxVjsgsK5ImLQ+4PRunO+M+MCasJ4g9gJuF211FfN5RfHoMrMg0tt+jHb/lymeuDUZa0NLffvYa5Uh55zb3f9p4WfQl37ZL71ptJXwtngNZ8+4IJgdB04t89/1O/w1cDnyilFU=';
-
+$access_token ='5BYUK9okWxGTtZujRx/DtuHl7kEGeQU9lyW+cuxVjsgsK5ImLQ+4PRunO+M+MCasJ4g9gJuF211FfN5RfHoMrMg0tt+jHb/lymeuDUZa0NLffvYa5Uh55zb3f9p4WfQl37ZL71ptJXwtngNZ8+4IJgdB04t89/1O/w1cDnyilFU=';
 $url = 'https://api.line.me/v1/oauth/verify';
 
-$headers = array('Authorization: Bearer ' . $access_token);
-
+$headers = array('Authorization: Bearer ' . $access_token);5BYUK9okWxGTtZujRx/DtuHl7kEGeQU9lyW+cuxVjsgsK5ImLQ+4PRunO+M+MCasJ4g9gJuF211FfN5RfHoMrMg0tt+jHb/lymeuDUZa0NLffvYa5Uh55zb3f9p4WfQl37ZL71ptJXwtngNZ8+4IJgdB04t89/1O/w1cDnyilFU=';
+ 
 $ch = curl_init($url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
@@ -12,4 +11,4 @@ curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
 $result = curl_exec($ch);
 curl_close($ch);
 
-echo $result;
+echo $result;NoNo
