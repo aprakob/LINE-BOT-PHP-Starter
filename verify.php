1245e67e2ee976a83a224427d74ed882b0cbc44f
aprakob <ownerlis83@gmail.com>
2017-07-12 19:09:16 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,3 +1,5 @@
+$proxyauth = 'username:password';'http://fixie:DDol4j7zXZ6m5i3@velodrome.usefixie.com:80
+โดนแ
 <?php
 $access_token = '5BYUK9okWxGTtZujRx/DtuHl7kEGeQU9lyW+cuxVjsgsK5ImLQ+4PRunO+M+MCasJ4g9gJuF211FfN5RfHoMrMg0tt+jHb/lymeuDUZa0NLffvYa5Uh55zb3f9p4WfQl37ZL71ptJXwtngNZ8+4IJgdB04t89/1O/w1cDnyilFU=';
 
@@ -9,6 +11,8 @@ $ch = curl_init($url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+curl_setopt($ch, CURLOPT_PROXY, $proxy);
+curl_setopt($ch, CURLOPT_PROXYUSERPWD, $proxyauth);
 $result = curl_exec($ch);
 curl_close($ch);
 
