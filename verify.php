325ba9b3f55d2a34d0c440a05b80c95bbcf6d48f
aprakob <ownerlis83@gmail.com>
2017-07-14 13:07:36 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,5 +1,4 @@
-$proxy = 'proxyurl:port';'80'
-$proxyauth = 'username:password';'http://fixie:DDol4j7zXZ6m5i3@velodrome.usefixie.com'
+$proxy = 'proxyurl:port';'80' $proxyauth = 'username:password';'http://fixie:DDol4j7zXZ6m5i3@velodrome.usefixie.com' 
 <?php
 $access_token = '5BYUK9okWxGTtZujRx/DtuHl7kEGeQU9lyW+cuxVjsgsK5ImLQ+4PRunO+M+MCasJ4g9gJuF211FfN5RfHoMrMg0tt+jHb/lymeuDUZa0NLffvYa5Uh55zb3f9p4WfQl37ZL71ptJXwtngNZ8+4IJgdB04t89/1O/w1cDnyilFU=';
 
