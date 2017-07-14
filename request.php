3167e761bbee944caf081e67ca486f6bb1e77287
aprakob <ownerlis83@gmail.com>
2017-07-14 13:11:29 +0700

    Update request.php

--- request.php
+++ request.php
@@ -2,7 +2,7 @@ curl -X POST \
 -H 'Content-Type:application/json' \
 -H 'Authorization: Bearer {ENTER_ACCESS_TOKEN}''5BYUK9okWxGTtZujRx/DtuHl7kEGeQU9lyW+cuxVjsgsK5ImLQ+4PRunO+M+MCasJ4g9gJuF211FfN5RfHoMrMg0tt+jHb/lymeuDUZa0NLffvYa5Uh55zb3f9p4WfQl37ZL71ptJXwtngNZ8+4IJgdB04t89/1O/w1cDnyilFU=' \
 -d '{
-    "replyToken":"",
+    "replyToken":"",+nHuyWiB7yP5Zw52FIkcQobQuGDXCTA
     "messages":[
         {
             "type":"text",
